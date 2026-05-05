bc15806b8ec320719fe68b36acb9285dfb9ccc87
Two-Serendra <twoserendra.scc@gmail.com>
2026-05-05 15:45:06 +0800

    hide grease trap

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -226,7 +226,7 @@
         @endroles
 
         @roles(1, 6)
-        <li class="menu-item">
+        <!-- <li class="menu-item">
             <a href="javascript:void(0);" class="menu-link menu-toggle">
                 <i class="menu-icon tf-icons bx bx-wrench"></i>
                 <div data-i18n="Form Elements">
@@ -257,7 +257,7 @@
                     </a>
                 </li>
             </ul>
-        </li>
+        </li> -->
 
         <li class="menu-item">
             <a href="javascript:void(0);" class="menu-link menu-toggle">

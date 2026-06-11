ba0e3cfad627d4da1b15c01fada9bca78f067b45
Two-Serendra <twoserendra.scc@gmail.com>
2026-06-11 11:05:47 +0800

    super app

--- resources/views/mobile-app/ausi-booking-mobile.blade.php
+++ resources/views/mobile-app/ausi-booking-mobile.blade.php
@@ -31,7 +31,7 @@
                                         <option value="" disabled>-- Select Residence --</option>
 
                                         <template x-for="(unit, index) in $store.superapp.units" :key="index">
-                                            <option :value="JSON.stringify(unit)"
+                                            <option :value="unit.name" :data-name="unit.name" :data-role="unit.role"
                                                 x-text="`${unit.role ?? ''} ${unit.name}`">
                                             </option>
                                         </template>
@@ -110,18 +110,18 @@
         </div>
 
         <div id="debugPanel" style="
-                            position: fixed;
-                            bottom: 0;
-                            left: 0;
-                            right: 0;
-                            height: 140px;
-                            overflow: auto;
-                            background: black;
-                            color: #00ff00;
-                            font-size: 11px;
-                            z-index: 99999;
-                            padding: 10px;
-                        ">
+                        position: fixed;
+                        bottom: 0;
+                        left: 0;
+                        right: 0;
+                        height: 140px;
+                        overflow: auto;
+                        background: black;
+                        color: #00ff00;
+                        font-size: 11px;
+                        z-index: 99999;
+                        padding: 10px;
+                    ">
         </div>
 
     </div>

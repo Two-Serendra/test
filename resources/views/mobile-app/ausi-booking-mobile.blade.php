3396c0cae6b31d08f446e5ab199ebb754522c1de
Two-Serendra <twoserendra.scc@gmail.com>
2026-06-15 14:15:44 +0800

    super app

--- resources/views/mobile-app/ausi-booking-mobile.blade.php
+++ resources/views/mobile-app/ausi-booking-mobile.blade.php
@@ -117,18 +117,18 @@
         </div>
 
         <div id="debugPanel" style="
-                                                        position: fixed;
-                                                        bottom: 0;
-                                                        left: 0;
-                                                        right: 0;
-                                                        height: 140px;
-                                                        overflow: auto;
-                                                        background: black;
-                                                        color: #00ff00;
-                                                        font-size: 11px;
-                                                        z-index: 99999;
-                                                        padding: 10px;
-                                                    ">
+                                                            position: fixed;
+                                                            bottom: 0;
+                                                            left: 0;
+                                                            right: 0;
+                                                            height: 140px;
+                                                            overflow: auto;
+                                                            background: black;
+                                                            color: #00ff00;
+                                                            font-size: 11px;
+                                                            z-index: 99999;
+                                                            padding: 10px;
+                                                        ">
         </div>
 
     </div>
@@ -161,6 +161,17 @@
                         showHome: false,
                     });
                 },
+                
+                onUnitChange(e) {
+                    const value = e.target.value;
+
+                    window.ausiState.unit = value;
+
+                    console.log("UNIT:", value);
+                    alert("UNIT CHANGED: " + value);
+
+                    triggerUpdate();
+                }
             }));
         });
     </script>

0779268c5922451de64c4abef57d661a6f0bd118
Two-Serendra <twoserendra.scc@gmail.com>
2026-06-16 10:09:46 +0800

    super app

--- public/assets/frontend/js/mobile-app/ausi-booking-mobile.js
+++ public/assets/frontend/js/mobile-app/ausi-booking-mobile.js
@@ -1,5 +1,5 @@
 $(function () {
-    alert("🔥 JS VERSION 2026-06-15-006");
+    alert("🔥 JS VERSION 2026-06-15-007");
     const el = document.getElementById('resident_id_ausi');
 
     alert("SELECT EXISTS: " + (el ? "YES" : "NO"));

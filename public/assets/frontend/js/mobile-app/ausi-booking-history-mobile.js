d77b31c651c551e85a94a8fdf2e9cc05382b5d4e
Two-Serendra <twoserendra.scc@gmail.com>
2026-06-17 14:07:35 +0800

    super app

--- public/assets/frontend/js/mobile-app/ausi-booking-history-mobile.js
+++ public/assets/frontend/js/mobile-app/ausi-booking-history-mobile.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    alert("🔥Ausi History JS VERSION 10");
+    alert("🔥Ausi History JS VERSION 11");
 
     const el = document.getElementById('resident_id_ausi_booking_history');
 

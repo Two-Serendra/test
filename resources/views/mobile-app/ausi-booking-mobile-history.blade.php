cbfb1c77c22d3b72657bf97ee87bd36a6695fd4c
Two-Serendra <twoserendra.scc@gmail.com>
2026-06-17 11:33:05 +0800

    super app

--- resources/views/mobile-app/ausi-booking-mobile-history.blade.php
+++ resources/views/mobile-app/ausi-booking-mobile-history.blade.php
@@ -19,10 +19,10 @@
                 <div class="row mb-3">
                     <div class="col-md-6 mb-3 mb-md-0">
                         <label class="form-label">Select Residence <span class="required">*</span></label>
-                        <input type="hidden" name="email" id="mobile_email">
-                        <input type="hidden" name="mobile_unit_name" id="mobile_unit_name">
-                        <input type="hidden" name="mobile_unit_role" id="mobile_unit_role">
-                        <input type="hidden" name="email" :value="$store.superapp.user?.email || ''">
+                        <input type="text" name="email" id="mobile_email">
+                        <input type="text" name="mobile_unit_name" id="mobile_unit_name">
+                        <input type="text" name="mobile_unit_role" id="mobile_unit_role">
+                        <input type="text" name="email" :value="$store.superapp.user?.email || ''">
                         <dl>
                             <dd>
 

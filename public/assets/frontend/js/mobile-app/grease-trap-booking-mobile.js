7c1025b4bbf519a405ccdc7bc7de471118cbf897
Two-Serendra <twoserendra.scc@gmail.com>
2026-06-25 11:30:39 +0800

    gt booking

--- public/assets/frontend/js/mobile-app/grease-trap-booking-mobile.js
+++ public/assets/frontend/js/mobile-app/grease-trap-booking-mobile.js
@@ -1,5 +1,5 @@
 $(function () {
-    alert("🔥 JS VERSION 2026-06-15-001");
+    alert("🔥GT BOOKING MOBILE JS VERSION 2026-06-15-002");
     const el = document.getElementById('resident_id_gt');
 
     logDebugGt("SELECT EXISTS: " + (el ? "YES" : "NO"));

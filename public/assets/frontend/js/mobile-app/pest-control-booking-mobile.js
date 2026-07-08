8bcd1a53a1e9d8c63975c6ff3d2d71505cf87ccb
Two-Serendra <twoserendra.scc@gmail.com>
2026-07-08 15:51:45 +0800

    mobile app

--- public/assets/frontend/js/mobile-app/pest-control-booking-mobile.js
+++ public/assets/frontend/js/mobile-app/pest-control-booking-mobile.js
@@ -1,5 +1,5 @@
 $(function () {
-    alert("🔥 Pest Control JS VERSION 2026-06-15-0011");
+    // alert("🔥 Pest Control JS VERSION 2026-06-15-0011");
     const el = document.getElementById('resident_id_pc');
 
     logDebugPc("SELECT EXISTS: " + (el ? "YES" : "NO"));

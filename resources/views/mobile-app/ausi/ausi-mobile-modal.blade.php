cbcc1c83c16ee84b22f66e1011680108cae89441
Two-Serendra <twoserendra.scc@gmail.com>
2026-07-14 14:56:28 +0800

    update

--- resources/views/mobile-app/ausi/ausi-mobile-modal.blade.php
+++ resources/views/mobile-app/ausi/ausi-mobile-modal.blade.php
@@ -66,7 +66,7 @@
 
                     <div class="card-body">
 
-                        <div class="row">
+                        <div class="row8 g-3">
 
                             <div class="col-md-6">
                                 <small class="text-muted d-block">

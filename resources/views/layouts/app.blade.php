9651aea696ff4ec90aa3a67904604b893bca8f83
Two-Serendra <twoserendra.scc@gmail.com>
2026-07-16 11:15:18 +0800

    mobile app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,7 +62,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body x-data style="padding-bottom:170px;">
+<body x-data>
     <div id="pageLoader" class="page-loader d-none">
         <div class="text-center">
             <div class="spinner-border text-primary mb-3" role="status"></div>

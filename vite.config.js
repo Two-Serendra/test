1904d5122b4a20fcf1c52991f23fa365092640dd
Two-Serendra <twoserendra.scc@gmail.com>
2026-08-12 14:01:35 +0800

    subway

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/pdf-viewer.js', 
             ],
             refresh: true,
         }),

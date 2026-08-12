a1e056fc5891023eecca45cf8ad78fcceb9f4331
Two-Serendra <twoserendra.scc@gmail.com>
2026-08-12 14:52:58 +0800

    mobile booking

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
     <link href="{{ asset('assets/frontend/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/frontend/css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/frontend/css/custom.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/mobile-app/ausi-booking-mobile.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/mobile-app/ausi-booking-mobile-v2.css') }}" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <!-- <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet"> -->
